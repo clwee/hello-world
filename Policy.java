2ac877d878f587f354d048b629d41cbe1ec6fc49
clwee <thaochristopherr@gmail.com>
2024-11-25 11:08:44 -0500

    Project 3
    "Added toString method"

--- Policy.java
+++ Policy.java
@@ -1,146 +1,59 @@
 public class Policy {
+    private static int policyCount = 0; 
 
-    
     private int policyNumber;
     private String providerName;
-    private String firstName;
-    private String lastName;
-    private int age;
-    private String smokingStatus;
-    private int height; 
-    private int weight; 
+    private PolicyHolder policyHolder;
 
-    
-    public Policy() {
-        this.policyNumber = 0;
-        this.providerName = "Unknown";
-        this.firstName = "Unknown";
-        this.lastName = "Unknown";
-        this.age = 0;
-        this.smokingStatus = "non-smoker";
-        this.height = 0;
-        this.weight = 0;
-    }
-
-    
-    public Policy(int policyNumber, String providerName, String firstName, String lastName, 
-                  int age, String smokingStatus, int height, int weight) {
+    public Policy(int policyNumber, String providerName, PolicyHolder policyHolder) {
         this.policyNumber = policyNumber;
         this.providerName = providerName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.smokingStatus = smokingStatus;
-        this.height = height;
-        this.weight = weight;
+        this.policyHolder = policyHolder;
+        policyCount++;
     }
 
-    
     public int getPolicyNumber() {
         return policyNumber;
+    }
+
+    public void setPolicyNumber(int policyNumber) {
+        this.policyNumber = policyNumber;
     }
 
     public String getProviderName() {
         return providerName;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getSmokingStatus() {
-        return smokingStatus;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    
-    public void setPolicyNumber(int policyNumber) {
-        this.policyNumber = policyNumber;
-    }
-
     public void setProviderName(String providerName) {
         this.providerName = providerName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public PolicyHolder getPolicyHolder() {
+        return policyHolder;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setPolicyHolder(PolicyHolder policyHolder) {
+        this.policyHolder = policyHolder;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public static int getPolicyCount() {
+        return policyCount;
     }
 
-    public void setSmokingStatus(String smokingStatus) {
-        this.smokingStatus = smokingStatus;
+    @Override
+    public String toString() {
+        return String.format("Policy Number: %d\nProvider Name: %s\n%sPolicy Price: $%.2f\n",
+                policyNumber, providerName, policyHolder.toString(), calculatePolicyPrice());
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    
-    public double calculateBMI() {
-        return (this.weight * 703.0) / (this.height * this.height);
-    }
-
-    
     public double calculatePolicyPrice() {
-        double baseFee = 600.0;
-        double totalPrice = baseFee;
-
-        
-        if (this.age > 50) {
-            totalPrice += 75.0;
+        double basePrice = 600;
+        if (policyHolder.isSmoker()) {
+            basePrice += 100;
         }
-
-        
-        if (this.smokingStatus.equalsIgnoreCase("smoker")) {
-            totalPrice += 100.0;
+        if (policyHolder.getAge() > 50) {
+            basePrice += 75;
         }
-
-        
-        double bmi = calculateBMI();
-        if (bmi > 35) {
-            totalPrice += (bmi - 35) * 20.0;
-        }
-
-        return totalPrice;
-    }
-
-    
-    public static void main(String[] args) {
-        
-        Policy defaultPolicy = new Policy();
-        System.out.println("Default Policy BMI: " + defaultPolicy.calculateBMI());
-        System.out.println("Default Policy Price: $" + defaultPolicy.calculatePolicyPrice());
-
-        
-        Policy customPolicy = new Policy(12345, "Health Insurance Inc.", "John", "Doe", 
-                                         55, "smoker", 70, 220);
-        System.out.println("Custom Policy BMI: " + customPolicy.calculateBMI());
-        System.out.println("Custom Policy Price: $" + customPolicy.calculatePolicyPrice());
+        return basePrice;
     }
 }
